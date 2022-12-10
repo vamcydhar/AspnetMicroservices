c2ae645644a7534146bfa9a6cff6789d72a85652
vamcydhar <vamcydhar@gmail.com>
2022-12-11 07:05:19 +0800

    Catalog.API microservice created

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration configuration;
+        public CatalogContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            var client = new MongoClient(this.configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(this.configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(this.configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
